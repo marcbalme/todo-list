545ebb3f1d22eeec42079e9e1e2b89f562b7c74d
Marc-grégoire <47780825+marcbalme@users.noreply.github.com>
2024-02-07 23:54:29 +0100

    fix header style

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -5,14 +5,16 @@ import { authConfig } from "@/pages/api/auth/[...nextauth]";
 import { LogoutButton } from "./LogoutButton";
 
 export const Header = async () => {
-  const session = await getServerSession(authConfig);
+    const session = await getServerSession(authConfig);
 
-  return (
-    <header className="flex max-w-screen-xl px-4 py-2 shadow-sm shadow-primary/50 ">
-      <div className="ml-auto ">
-        {session ? <LogoutButton /> : <LoginButton />}
-        <ThemeSwitch />
-      </div>
-    </header>
-  );
+    return (
+        <header className="shadow-sm shadow-primary/50 ">
+            <div className="max-w-screen-xl px-4 py-2 flex">
+                <div className="ml-auto">
+                    {session ? <LogoutButton /> : <LoginButton />}
+                    <ThemeSwitch />
+                </div>
+            </div>
+        </header>
+    );
 };
