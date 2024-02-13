6a029abb1eb169d7134f3e7873272039d1f8fd01
Marc-grégoire <47780825+marcbalme@users.noreply.github.com>
2024-02-14 00:42:08 +0100

    add loading state

--- app/_components/TodoList.tsx
+++ app/_components/TodoList.tsx
@@ -1,8 +1,9 @@
 import prisma from "@/lib/prisma";
 import { TodoItem } from "./TodoItem";
 import { Todo } from "@prisma/client";
+import { Skeleton } from "@/components/ui/skeleton";
 
-export const TodoList = async () => {
+const TodoList = async () => {
     const todos: Array<Todo> = await prisma.todo.findMany({
         orderBy: {
             createdAt: "desc",
@@ -21,3 +22,22 @@ export const TodoList = async () => {
         </ul>
     );
 };
+
+TodoList.Skeleton = function SkeletonTodoList(): JSX.Element {
+    return (
+        <div className="flex flex-col pt-8">
+            <Skeleton className="h-14 w-full p-2 mb-4" />
+            <Skeleton className="h-14 w-full p-2 mb-4" />
+            <Skeleton className="h-14 w-full p-2 mb-4" />
+            <Skeleton className="h-14 w-full p-2 mb-4" />
+            <Skeleton className="h-14 w-full p-2 mb-4" />
+            <Skeleton className="h-14 w-full p-2 mb-4" />
+            <Skeleton className="h-14 w-full p-2 mb-4" />
+            <Skeleton className="h-14 w-full p-2 mb-4" />
+        </div>
+    );
+};
+
+TodoList.displayName = "TodoList";
+
+export { TodoList };
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import { AddItemForm } from "./_components/AddItemForm";
 import { TodoList } from "./_components/TodoList";
 
@@ -5,7 +6,9 @@ export default function Home() {
     return (
         <main className="flex min-h-screen flex-col p-6 max-w-screen-sm mx-auto">
             <AddItemForm />
-            <TodoList />
+            <Suspense fallback={<TodoList.Skeleton />}>
+                <TodoList />
+            </Suspense>
         </main>
     );
 }
